e2ee4de7f1158063ad96ed46f5886ede6496cbb6
Anvita Gollu <75912590+anvita-g@users.noreply.github.com>
2023-03-16 12:09:43 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
 
   <div class ="card flex flex-column md:flex-row gap-3">
     <div class="p-inputgroup flex-1">
-      <InputText type="text" v-model="value" />
+      <span class="p-inputgroup-addon">
+        <i class="pi pi-user"></i>
+      </span>
+      <InputText placeholder = "Username" type="text" v-model="value" />
     </div>
   </div>
 
@@ -14,4 +17,3 @@ export default {
   name: "App"
 }
 </script>
-
